6ae52a35fdf19f8730bf2c15f7dfe58a90ec015a
Anthony Brogni <anthonybrog@gmail.com>
2025-01-05 18:22:45 -0600

    Clean up unused imports

--- src/main/java/frc/robot/commands/CoralElevatorMoveCommand.java
+++ src/main/java/frc/robot/commands/CoralElevatorMoveCommand.java
@@ -3,8 +3,6 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.Robot;
--- src/main/java/frc/robot/commands/CoralElevatorScoreHighCommand.java
+++ src/main/java/frc/robot/commands/CoralElevatorScoreHighCommand.java
@@ -3,10 +3,7 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.subsystems.CoralElevatorSubsystem;
 
