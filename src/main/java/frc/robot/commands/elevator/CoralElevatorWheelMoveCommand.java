ae82e7aeef93b594b159796a9590f94b46ae980f
IsaacPatton <23pattisa@isd748.org>
2025-01-06 22:20:29 -0600

    folderize everything

--- src/main/java/frc/robot/commands/elevator/CoralElevatorWheelMoveCommand.java
+++ src/main/java/frc/robot/commands/elevator/CoralElevatorWheelMoveCommand.java
@@ -1,18 +1,21 @@
 // Author: UMN Robotics Ri3D
 // Last Updated: January 2025
 
-package frc.robot.commands;
+package frc.robot.commands.elevator;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.Robot;
 import frc.robot.subsystems.CoralElevatorSubsystem;
 
-// This command activates the Horizontal Elevator/End Effector preset
-public class CoralElevatorHorizontalIntakeCommand extends Command {
+// This Command causes the elevator to ascend or descend
+public class CoralElevatorWheelMoveCommand extends Command {
   private CoralElevatorSubsystem m_subsystem;
+  private boolean direction;
 
-  /** D Pad down command, sets height to Neutral and End Effector to Horizontal */
-  public CoralElevatorHorizontalIntakeCommand() {
+  /** Right Bumper command, causes Elevator to ascend. Left Bumper command, causes Elevator to descend */
+  public CoralElevatorWheelMoveCommand(boolean direction) {
+    this.direction = direction;
     m_subsystem = Robot.m_CoralElevatorSubsystem;
     addRequirements(m_subsystem);
   }
@@ -20,8 +23,7 @@ public class CoralElevatorHorizontalIntakeCommand extends Command {
   // Called once when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_subsystem.armHorizontalIntake();
-    m_subsystem.climbNeutral();
+    m_subsystem.setSpeedWheel(this.direction ? -1 * Constants.WHEEL_SPEED : Constants.WHEEL_SPEED);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -33,12 +35,12 @@ public class CoralElevatorHorizontalIntakeCommand extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    // -
+    m_subsystem.stopWheel();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return true; // Command will finish immediately
+    return false; // Command will never finish (we don't want it to)
   }
 }
